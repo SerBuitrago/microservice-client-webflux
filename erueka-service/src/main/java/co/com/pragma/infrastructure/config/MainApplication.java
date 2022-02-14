cb5c1fee3fdce7f008351b5cad0f003f603bfd29
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-02-14 13:38:51 -0500

    Add Congig server

--- erueka-service/src/main/java/co/com/pragma/infrastructure/config/MainApplication.java
+++ erueka-service/src/main/java/co/com/pragma/infrastructure/config/MainApplication.java
@@ -2,12 +2,14 @@ package co.com.pragma.infrastructure.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class EruekaServiceApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EruekaServiceApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 
 }
